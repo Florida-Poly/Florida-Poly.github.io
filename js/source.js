e8dc81e35097de87375113dc0dc5bbf12c968476
James Andrews <jandrews7348@floridapoly.edu>
2020-06-22 11:41:11 -0400

    Update source.js

--- js/source.js
+++ js/source.js
@@ -7,7 +7,7 @@ function hack(){
 		for (var i = 0, n = charset.length; i < length; ++i) {
 			passcode += charset.charAt(Math.floor(Math.random() * n));
 		}
-		var key = "Your code:" + passcode
+		var key = "Your code:  " + passcode
 		alert(key);
 	}
 }
