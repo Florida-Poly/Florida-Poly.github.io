8b94c7f45ccd2b31b15295d43fd619286faaefac
James Andrews <jandrews7348@floridapoly.edu>
2020-07-31 12:43:10 -0400

    fixed?

--- js/source.js
+++ js/source.js
@@ -1,1 +1,11 @@
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('1 a=3;e c(){d(a==3){1 2=8,4="f",5="";b(1 i=0,9=4.2;i<2;++i){5+=4.g(6.k(6.j()*9))}1 7="h l:  "+5 m(7)}}',23,23,'|var|length||charset|passcode|Math|key||n|level|for|hack|if|function|125689|charAt|Your||random|floor|code|alert'.split('|'),0,{}))
+var level = 3;
+function hack(){
+  var  length = 8,
+    charset = "125689",
+    passcode = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    passcode += charset.charAt(Math.floor(Math.random() * n));
+  }
+  var key = "Your code:  " + passcode
+  alert(key);
+}
